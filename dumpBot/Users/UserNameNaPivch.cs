de3816fb1f03ee5112abf87a2729215cd0f2690c
Vasterlor <sashakuzo1807@gmail.com>
2023-10-19 21:19:48 +0300

    add new class UserNameNaPivch

---
+++ dumpBot/Users/UserNameNaPivch.cs
@@ -0,0 +1,12 @@
+﻿namespace dumpBot.Users;
+
+public class UserNameNaPivch
+{
+    public string sashakuzo = "@sashakuzo";
+    public string hroshko_p = "@hroshko_p";
+    public string roonua1 = "@roonua1";
+    public string Healermanrober = "@Healermanrober";
+    public string Kostya = "@Kostya";
+    public string Рузана = "@Рузана";
+    public string iamfuss = "@iamfuss";
+}
