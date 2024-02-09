1cb14e5a805db690be84f8f6f03a41def5aeb5cc
sasha <sasha@DARKMASTER>
2024-02-09 17:45:21 +0200

    test commit

--- dumpBot/obj/Debug/net7.0/dumpBot.AssemblyInfo.cs
+++ dumpBot/obj/Debug/net7.0/dumpBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dumpBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3633f15a29803ea13a33fd53cee266f07797dc87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("dumpBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dumpBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
